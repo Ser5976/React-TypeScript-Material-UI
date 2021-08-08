71710eb9c0593cc8222743cc7ee860803edac664
Seriy <tsbaranova@bk.ru>
2021-08-08 18:04:26 +0300

    urls

--- src/constant/urls.ts
+++ src/constant/urls.ts
@@ -1,7 +1,10 @@
-//let ROOT_URL = 'http://localhost:4000';
 export const ROOT_URL = 'http://localhost:5000';
 
 export const ModelUrls = {
   TODOLIST: `${ROOT_URL}/api/todoList/`,
   NOTES: `${ROOT_URL}/api/notes/`,
+};
+export const MOVIE_API_URL = {
+  ROOT_URL: 'https://www.omdbapi.com/',
+  KEY: '4a3b711b',
 };
